aea7b2c3c6f63e736cf74af6eba9a77bee1c27e7
Jon Bracy <jonbracy@gmail.com>
2017-01-11 13:12:44 -0800

    Fix and issue with Rails caching

--- lib/cached_at/reflections/abstract_reflection.rb
+++ lib/cached_at/reflections/abstract_reflection.rb
@@ -1,29 +1,41 @@
 module CachedAt
   module AbstractReflection
   
-    def through_relationship_endpoints
-      return @through_relationship_endpoints if instance_variable_defined?(:@through_relationship_endpoints)
-    
-      @through_relationship_endpoints = []
-      active_record.reflections.each do |name, r|
-        next if r == self
-        begin
-          if r.polymorphic?
-            # TODO
-          else
-            r.klass.reflections.each do |name2, r2|
-              if r2.options[:cached_at] && r2.options[:through] && r2.options[:through] == r.inverse_of&.name
-                @through_relationship_endpoints << r2
-              end
-            end
-          end
-        rescue NameError
-        end
+    def cache_through_relationship_endpoints
+      if defined?(Rails)
+        Rails.application.config.cache_classes
+      else
+        true
       end
+    end
     
+    def through_relationship_endpoints
+      return @through_relationship_endpoints if instance_variable_defined?(:@through_relationship_endpoints) && cache_through_relationship_endpoints
+
+      @through_relationship_endpoints = if defined?(Rails)
+        Rails.application.reloader.wrap { calculate_through_relationship_endpoints }
+      else
+        calculate_through_relationship_endpoints
+      end
+
       @through_relationship_endpoints
     end
   
+    def calculate_through_relationship_endpoints
+      endpoints = []
+      
+      if self.polymorphic?
+      else
+        self.klass._reflections.each do |name, r|
+          if r.options[:cached_at] && r.options[:through] && r.options[:through] == self.inverse_of&.name
+            endpoints << r
+          end
+        end
+      end
+
+      endpoints
+    end
+    
   end
 end
 
