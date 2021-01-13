290b5a3b030f2939c2977ffa2dcb85ee3b4a9500
Jon Bracy <jonbracy@gmail.com>
2021-01-13 13:30:14 -0600

    More Rails 6.1 changes

--- ext/active_record/connection_adapters/abstract/schema_statements.rb
+++ ext/active_record/connection_adapters/abstract/schema_statements.rb
@@ -5,6 +5,10 @@ module ActiveRecord
       def add_timestamps(table_name, **options)
         options[:null] = false if options[:null].nil?
 
+        if !options.key?(:precision) && supports_datetime_with_precision?
+          options[:precision] = 6
+        end
+
         add_column table_name, :created_at, :datetime, **options
         add_column table_name, :updated_at, :datetime, **options
         add_column table_name, :cached_at, :datetime,  **options
@@ -15,6 +19,25 @@ module ActiveRecord
         remove_column table_name, :created_at
         remove_column table_name, :cached_at
       end
+
+      def add_timestamps_for_alter(table_name, **options)
+        options[:null] = false if options[:null].nil?
+
+        if !options.key?(:precision) && supports_datetime_with_precision?
+          options[:precision] = 6
+        end
+
+        [
+          add_column_for_alter(table_name, :created_at, :datetime, **options),
+          add_column_for_alter(table_name, :updated_at, :datetime, **options),
+          add_column_for_alter(table_name, :cached_at, :datetime, **options)
+        ]
+      end
+
+      def remove_timestamps_for_alter(table_name, **options)
+        remove_columns_for_alter(table_name, :updated_at, :created_at, :cached_at)
+      end
+
     end
 
   end
--- lib/cached_at/timestamp.rb
+++ lib/cached_at/timestamp.rb
@@ -7,11 +7,13 @@ module ActiveRecord
         private
     
         def timestamp_attributes_for_update
-          ['updated_at']
+          ["updated_at", "updated_on", 'cached_at'].map! { |name| attribute_aliases[name] || name }
         end
 
         def timestamp_attributes_for_create
-          ['created_at', 'udpated_at', 'cached_at'] + column_names.select{|c| c.end_with?('_cached_at') }
+          (['created_at', 'udpated_at', 'cached_at'] + column_names.select{|c| c.end_with?('_cached_at') }).map! do |name|
+            attribute_aliases[name] || name
+          end
         end
 
       end
